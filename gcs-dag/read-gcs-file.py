d654db1b52fff3b49d0b23954018d1b1dfaa5d25
yurgas <yurarsit@gmail.com>
2021-04-30 13:45:15 +0300

    Update read-gcs-file.py

--- gcs-dag/read-gcs-file.py
+++ gcs-dag/read-gcs-file.py
@@ -13,10 +13,4 @@ with models.DAG(
     schedule_interval=None,
     tags=['example'],
 ) as dag:
-    # [START howto_operator_gcs_download_file_task]
-    download_file = GCSToLocalFilesystemOperator(
-        task_id="download_file",
-        bucket="my-test-airflow-gcs-bucket",
-        filename="test-gcs-example-local.txt",
-    )
-    # [END howto_operator_gcs_download_file_task]
+    download_file = GCSToLocalFilesystemOperator(task_id="download_file",object_name="test-gcs-example-local.txt",bucket="my-test-airflow-gcs-bucket",filename="test-gcs-example-local.txt",)
