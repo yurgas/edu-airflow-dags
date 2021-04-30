a8922fa2c95474635382cdf18e10700fee9e5bb7
yurgas <yurarsit@gmail.com>
2021-04-30 14:03:04 +0300

    Update read-gcs-file.py

--- gcs-dag/read-gcs-file.py
+++ gcs-dag/read-gcs-file.py
@@ -1,16 +1,29 @@
-import os
-
-from airflow import models
+from airflow import DAG
 from airflow.models import Variable
 from airflow.providers.google.cloud.transfers.gcs_to_local import GCSToLocalFilesystemOperator
 from airflow.utils.dates import days_ago
 
+args = {
+    'owner': 'airflow',
+}
+
 PROJECT_ID = Variable.get("gcp-project-id")
 
-with models.DAG(
-    "example_gcs_to_local4",
+dag = DAG(
+    dag_id='example_gcs_to_local5',
+    default_args=args,
+    schedule_interval='0 0 * * *',
     start_date=days_ago(1),
-    schedule_interval=None,
-    tags=['example'],
-) as dag:
-    download_file = GCSToLocalFilesystemOperator(task_id="download_file",bucket="my-test-airflow-gcs-bucket",filename="test-gcs-example-local.txt")
+    dagrun_timeout=timedelta(minutes=60),
+)
+
+download_file = GCSToLocalFilesystemOperator(
+    task_id='download_file',
+    object_name='test-gcs-example-local.txt',
+    bucket='my-test-airflow-gcs-bucket',
+    filename='test-gcs-example-local.txt',
+    dag=dag,
+)
+
+if __name__ == "__main__":
+    dag.cli()
