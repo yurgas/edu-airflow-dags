0c2b5f031d7706a54b0b7f0cae69720d4146b26c
yurgas <yurarsit@gmail.com>
2021-04-30 15:13:16 +0300

    Copy

--- gcs-dag/read-gcs-file2.py
+++ gcs-dag/read-gcs-file2.py
@@ -11,7 +11,7 @@ args = {
 PROJECT_ID = Variable.get("gcp-project-id")
 
 dag = DAG(
-    dag_id='example_gcs_to_local6',
+    dag_id='example_gcs_to_local7',
     default_args=args,
     schedule_interval='0 0 * * *',
     start_date=days_ago(1),
@@ -19,11 +19,11 @@ dag = DAG(
 )
 
 download_file = GCSToLocalFilesystemOperator(
-    task_id='download_file',
+    task_id='download_file2',
+    object_name='test-gcs-example-local.txt',
     gcp_conn_id='my_gcp',
     bucket='my-test-airflow-gcs-bucket',
     filename='test-gcs-example-local.txt',
-    object_name='test-gcs-example-local.txt',
     dag=dag,
 )
 
