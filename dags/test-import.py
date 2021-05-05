3e8d64ab9d149c4be4cb198867d9ce7fd4626525
yurgas <yurarsit@gmail.com>
2021-05-05 11:31:05 +0300

    Add changes

---
+++ dags/test-import.py
@@ -0,0 +1,30 @@
+"""Python test import."""
+
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+from shared.test_module.test_module import TestClass
+
+args = {
+    'owner': 'airflow',
+}
+
+dag = DAG(
+    dag_id='test_import_dag',
+    default_args=args,
+    schedule_interval='0 0 * * *',
+    start_date=days_ago(2),
+    dagrun_timeout=timedelta(minutes=60),
+)
+
+run_this = BashOperator(
+    task_id='echo_hello',
+    bash_command='echo "Hello there"',
+    dag=dag,
+)
+
+if __name__ == "__main__":
+    dag.cli()
