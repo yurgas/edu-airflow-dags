d60116cf04dc0d52a6b880583ae1ca800aba04a2
yurgas <yurarsit@gmail.com>
2021-09-27 14:57:19 +0300

    Add sleep dag

---
+++ dags/sleep-dag.py
@@ -0,0 +1,28 @@
+"""Python version validation."""
+
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+args = {
+    'owner': 'airflow',
+}
+
+dag = DAG(
+    dag_id='sleep_dag',
+    default_args=args,
+    schedule_interval='0 0 * * *',
+    start_date=days_ago(2),
+    dagrun_timeout=timedelta(minutes=60),
+)
+
+run_this = BashOperator(
+    task_id='sleep_5m',
+    bash_command='sleep 300',
+    dag=dag,
+)
+
+if __name__ == "__main__":
+    dag.cli()
