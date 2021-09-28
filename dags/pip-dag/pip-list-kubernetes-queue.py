392cc778c9a9b90f862114b1d4f3775dbb2ca28d
yurgas <yurarsit@gmail.com>
2021-09-28 12:02:42 +0300

    Add pip list in kubernetes queue

---
+++ dags/pip-dag/pip-list-kubernetes-queue.py
@@ -0,0 +1,29 @@
+"""Python version validation."""
+
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+args = {
+    'owner': 'airflow',
+}
+
+dag = DAG(
+    dag_id='pip_list_dag',
+    default_args=args,
+    schedule_interval='0 0 * * *',
+    start_date=days_ago(2),
+    dagrun_timeout=timedelta(minutes=60),
+)
+
+pip_list = BashOperator(
+    task_id='get_pip_list',
+    bash_command='pip version; pip list',
+    dag=dag,
+    queue="kubernetes"
+)
+
+if __name__ == "__main__":
+    dag.cli()
